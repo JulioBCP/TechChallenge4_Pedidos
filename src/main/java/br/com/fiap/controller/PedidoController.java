296aaba64150987df982f4f658a94529c54ae5b5
Julio <juliobcp@gmail.com>
2024-05-01 19:14:54 -0300

    Primeiro commit do microsservico Pedidos

---
+++ src/main/java/br/com/fiap/controller/PedidoController.java
@@ -0,0 +1,40 @@
+package br.com.fiap.controller;
+
+import br.com.fiap.model.Pedido;
+import br.com.fiap.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @PostMapping
+    public ResponseEntity<?> criarPedido(@RequestBody Pedido pedido) {
+        try {
+            Pedido novoPedido = pedidoService.criarPedido(pedido);
+            return new ResponseEntity<>(novoPedido, HttpStatus.CREATED);
+        } catch(NoSuchElementException e) {
+            return new ResponseEntity<>("Um ou mais produtos nao estao disponiveis", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public List<Pedido> listarPedidos() {
+        return pedidoService.listarPedidos();
+    }
+
+    @DeleteMapping("/{pedidoId}")
+    public void cancelarPedido(@PathVariable Integer pedidoId) {
+        pedidoService.cancelarPedido(pedidoId);
+    }
+
+}
